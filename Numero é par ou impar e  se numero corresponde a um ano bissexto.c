4324372169f4f7ce448ecba8963b7a4827b4b638
David-Capulot-Correa <davidcapulot2025@gmail.com>
2025-10-11 11:37:08 -0300

    Add files via upload

---
+++ Numero é par ou impar e  se numero corresponde a um ano bissexto.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+    int opcao;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+    printf("\nEscolha uma opção:\n");
+    printf("1 - Saber se o número é par ou ímpar\n");
+    printf("2 - Saber se o número corresponde a um ano bissexto\n");
+    printf("Opção: ");
+    scanf("%d", &opcao);
+
+    if (opcao == 1) {
+        // Verifica se é par ou ímpar
+        if (numero % 2 == 0) {
+            printf("\nO número %d é PAR.\n", numero);
+        } else {
+            printf("\nO número %d é ÍMPAR.\n", numero);
+        }
+
+    } else if (opcao == 2) {
+        // Verifica se é ano bissexto
+        if ((numero % 400 == 0) || (numero % 4 == 0 && numero % 100 != 0)) {
+            printf("\nO ano %d é BISSEXTO.\n", numero);
+        } else {
+            printf("\nO ano %d NÃO é bissexto.\n", numero);
+        }
+
+    } else {
+        printf("\nOpção inválida!\n");
+    }
+
+    return 0;
+}
