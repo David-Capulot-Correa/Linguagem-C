de3ec531d35204ffbc8eed829b0cbc0d5fc1a8e6
David-Capulot-Correa <davidcapulot2025@gmail.com>
2026-02-15 17:09:13 -0300

    Update Jogo da Forca .c

--- Jogo da Forca .c
+++ Jogo da Forca .c
@@ -1,134 +1,169 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
-
-#define MAX_TENTATIVAS 6
-
-
-void desenharForca(int erros) {
-    printf("\n");
-
-    if (erros == 0) {
-        printf("  +---+\n");
-        printf("  |   |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("=========\n");
-    }
-    else if (erros == 1) {
-        printf("  +---+\n");
-        printf("  |   |\n");
-        printf("  O   |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("=========\n");
-    }
-    else if (erros == 2) {
-        printf("  +---+\n");
-        printf("  |   |\n");
-        printf("  O   |\n");
-        printf("  |   |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("=========\n");
-    }
-    else if (erros == 3) {
-        printf("  +---+\n");
-        printf("  |   |\n");
-        printf("  O   |\n");
-        printf(" /|   |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("=========\n");
-    }
-    else if (erros == 4) {
-        printf("  +---+\n");
-        printf("  |   |\n");
-        printf("  O   |\n");
-        printf(" /|\\  |\n");
-        printf("      |\n");
-        printf("      |\n");
-        printf("=========\n");
-    }
-    else if (erros == 5) {
-        printf("  +---+\n");
-        printf("  |   |\n");
-        printf("  O   |\n");
-        printf(" /|\\  |\n");
-        printf(" /    |\n");
-        printf("      |\n");
-        printf("=========\n");
-    }
-    else {
-        printf("  +---+\n");
-        printf("  |   |\n");
-        printf("  O   |\n");
-        printf(" /|\\  |\n");
-        printf(" / \\  |\n");
-        printf("      |\n");
-        printf("=========\n");
-    }
-}
-
+#include <string.h>
 
 int main() {
 
-    char palavras[][20] = {"computador", "programacao", "teclado", "internet", "algoritmo"};
-    int totalPalavras = 5;
+    char palavras[10][30] = {
+        "programacao",
+        "computador",
+        "memoria",
+        "algoritmo",
+        "compilador",
+        "estrutura",
+        "variavel",
+        "software",
+        "hardware",
+        "internet"
+    };
 
     srand(time(NULL));
-    int indice = rand() % totalPalavras;
+    int indice = rand() % 10;
 
-    char palavra[20];
-    strcpy(palavra, palavras[indice]);
+    char palavra[30];
+    int i = 0;
+
+    while (palavras[indice][i] != '\0') {
+        palavra[i] = palavras[indice][i];
+        i++;
+    }
+    palavra[i] = '\0';
 
     int tamanho = strlen(palavra);
 
-    char descoberta[20];
-    for (int i = 0; i < tamanho; i++) {
+    char descoberta[30];
+    for (i = 0; i < tamanho; i++) {
         descoberta[i] = '_';
     }
     descoberta[tamanho] = '\0';
 
+    char usadas[26];
+    int totalUsadas = 0;
+
     int erros = 0;
+    int maxErros = 6;
     char letra;
-    int acertou;
 
-    while (erros < MAX_TENTATIVAS && strcmp(palavra, descoberta) != 0) {
+    printf("===== JOGO DA FORCA =====\n");
+    printf("Tema: Tecnologia\n");
 
-        desenharForca(erros);
+    while (erros < maxErros) {
 
-        printf("\nPalavra: %s\n", descoberta);
-        printf("Tentativas restantes: %d\n", MAX_TENTATIVAS - erros);
+        int ganhou = 1;
+        for (i = 0; i < tamanho; i++) {
+            if (descoberta[i] == '_') {
+                ganhou = 0;
+                break;
+            }
+        }
 
-        printf("Digite uma letra: ");
+        if (ganhou == 1) {
+            break;
+        }
+
+        // DESENHO DA FORCA
+        printf("\n  +---+\n");
+        printf("  |   |\n");
+
+        if (erros >= 1)
+            printf("  O   |\n");
+        else
+            printf("      |\n");
+
+        if (erros == 2)
+            printf("  |   |\n");
+        else if (erros == 3)
+            printf(" /|   |\n");
+        else if (erros >= 4)
+            printf(" /|\\  |\n");
+        else
+            printf("      |\n");
+
+        if (erros == 5)
+            printf(" /    |\n");
+        else if (erros >= 6)
+            printf(" / \\  |\n");
+        else
+            printf("      |\n");
+
+        printf("      |\n");
+        printf("=========\n");
+
+        printf("\nErros: %d\n", erros);
+        printf("Palavra: %s\n", descoberta);
+
+        printf("Letras usadas: ");
+        for (i = 0; i < totalUsadas; i++) {
+            printf("%c ", usadas[i]);
+        }
+
+        printf("\nDigite uma letra: ");
         scanf(" %c", &letra);
 
-        acertou = 0;
+        int repetida = 0;
+        for (i = 0; i < totalUsadas; i++) {
+            if (usadas[i] == letra) {
+                repetida = 1;
+            }
+        }
 
-        for (int i = 0; i < tamanho; i++) {
+        if (repetida == 1) {
+            printf("Letra ja usada!\n");
+            continue;
+        }
+
+        usadas[totalUsadas] = letra;
+        totalUsadas++;
+
+        int acertou = 0;
+
+        for (i = 0; i < tamanho; i++) {
             if (palavra[i] == letra) {
                 descoberta[i] = letra;
                 acertou = 1;
             }
         }
 
-        if (!acertou) {
+        if (acertou == 0) {
             erros++;
         }
     }
 
-    desenharForca(erros);
+    // DESENHO FINAL
+    printf("\n  +---+\n");
+    printf("  |   |\n");
 
-    if (strcmp(palavra, descoberta) == 0) {
-        printf("\n🎉 Voce ganhou! A palavra era: %s\n", palavra);
-    } else {
-        printf("\n💀 Voce perdeu! A palavra era: %s\n", palavra);
-    }
+    if (erros >= 1)
+        printf("  O   |\n");
+    else
+        printf("      |\n");
+
+    if (erros == 2)
+        printf("  |   |\n");
+    else if (erros == 3)
+        printf(" /|   |\n");
+    else if (erros >= 4)
+        printf(" /|\\  |\n");
+    else
+        printf("      |\n");
+
+    if (erros == 5)
+        printf(" /    |\n");
+    else if (erros >= 6)
+        printf(" / \\  |\n");
+    else
+        printf("      |\n");
+
+    printf("      |\n");
+    printf("=========\n");
+
+    if (erros == maxErros)
+        printf("\nVoce perdeu!\n");
+    else
+        printf("\nParabens! Voce ganhou!\n");
+
+    printf("Palavra correta: %s\n", palavra);
 
     return 0;
 }
